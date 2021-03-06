fc8c4179ae899bdf5621af557fad25c5b32d643f
tommy.huang <cy94295@gmail.com>
2021-03-06 13:35:01 +0800

    add race condition

---
+++ race_condition/race.js
@@ -0,0 +1,19 @@
+let value = 1;
+
+const waitFunction = async () => {
+  console.log({ value });
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+  await promise;
+  console.log({ value });
+};
+
+const setFunction = () => {
+  value = 2;
+};
+
+waitFunction();
+setFunction();
