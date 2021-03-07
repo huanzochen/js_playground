b9cf99e1a614262198ddd896c88ad33559c2e57e
tommy.huang <tommy.huang@wavenet.com.tw>
2021-03-07 20:24:44 +0800

    change file position & add ch2 ch3 practice 1. change race filfe position 2. add promise_scheduling 3. add thenable_duck_typing

---
+++ ch3/thenable_duck_typing.js
@@ -0,0 +1,16 @@
+var o = { then: function(){
+    console.log('this is a duck typing then() function');
+} };
+
+// make `v` be `[[Prototype]]`-linked to `o`
+var v = Object.create( o );
+
+v.someStuff = "cool";
+v.otherStuff = "not so cool";
+
+console.log(v.hasOwnProperty( "then" ));	
+
+// not in here
+v.then(function () {
+    console.log('in v.then() now')
+})
