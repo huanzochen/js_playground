7ed28ee7d00fd9a0ce964b994dc0e3dc02d869ef
tommy.huang <tommy.huang@wavenet.com.tw>
2021-03-08 00:10:35 +0800

    add fullfillment & rejection handler

---
+++ ch3/default_rejection_handler.js
@@ -0,0 +1,11 @@
+var p = new Promise(function(resolve, reject) {
+  reject('Oops')
+})
+var p2 = p
+  .then(function fullfilled() {
+    console.log('不會到這裡')
+  }
+    //   , function error(err) {
+    // console.log(`會到這裡, 錯誤為 ${err}`)
+    //   }
+  )
