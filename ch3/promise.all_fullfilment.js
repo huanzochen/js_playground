ff6eda89b8dd4f15549a10c8cc9c0eb006208673
tommy.huang <tommy.huang@wavenet.com.tw>
2021-03-08 23:03:56 +0800

    add promise.all_fullfillment

---
+++ ch3/promise.all_fullfilment.js
@@ -0,0 +1,29 @@
+function p1 (time) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      console.log('i want to do somthing in p1')
+      resolve('p1 resoved message')
+    }, time)
+  })
+}
+
+function p2 (time) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      console.log('i want to do somthing in p2')
+      resolve('p2 resoved message')
+    }, time)
+  })
+}
+
+Promise.all([p1(1000), p2(2000)])
+  .then(function(values) {
+    // fullfillment
+    console.log('it has been fillfilled')
+    console.log(values)
+  }, function(err) {
+    //rejection
+    console.log('it has been rejected')
+    console.log(err)
+  })
+
