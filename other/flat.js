c0424fe8f1d985d3bc695a9e58e322601def453e
tommy.huang <cy94295@gmail.com>
2021-03-30 20:57:25 +0800

    簡化函式

--- other/flat.js
+++ other/flat.js
@@ -18,26 +18,28 @@ function flatten() {
  * arr.reduce(callback[accumulator, currentValue, currentIndex, array], initialValue)
  */
 
+// function flattenDeep(target) {
+//   return target.reduce((accumulator, currentValue, currentIndex, originArray) => {
+//     console.log('accumulator =')
+//     console.log(accumulator)
+//     console.log('currentValue =')
+//     console.log(currentValue)
+//     console.log(`currentIndex = ${currentIndex}`)
+//     console.log('originArray =')
+//     console.log(originArray)
+//     console.log(`is it array? ${Array.isArray(currentValue)}`)
+//     if (Array.isArray(currentValue)) {
+//       return accumulator = accumulator.concat(flattenDeep(currentValue))
+//     }
+//     else {
+//       return accumulator = accumulator.concat(currentValue)
+//     }
+//   }, [])  
+// }
+
 function flattenDeep(target) {
-  return target.reduce((accumulator, currentValue, currentIndex, originArray) => {
-    console.log('accumulator =')
-    console.log(accumulator)
-    console.log('currentValue =')
-    console.log(currentValue)
-    console.log(`currentIndex = ${currentIndex}`)
-    console.log('originArray =')
-    console.log(originArray)
-    console.log(`is it array? ${Array.isArray(currentValue)}`)
-    if (Array.isArray(currentValue)) {
-      return accumulator = accumulator.concat(flattenDeep(currentValue))
-    }
-    else {
-      return accumulator = accumulator.concat(currentValue)
-    }
-
-  }, [])  
-
-}
+    return target.reduce((accumulator, currentValue, currentIndex, originArray) => Array.isArray(currentValue) ? accumulator.concat(flattenDeep(currentValue)) : accumulator.concat(currentValue), [])
+  }
 
 
 let result = flattenDeep(arr2)
