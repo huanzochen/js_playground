c8ec577d76219179b7301e2d96d129a98327a59e
tommy.huang <cy94295@gmail.com>
2021-04-14 12:27:55 +0800

    add function expression and setTimeOut

---
+++ other/function-expression.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function map(f, a) {
+  var result = []
+  var i
+  for (let i = 0;i != a.length;i++) {
+    result[i] = f(a[i])
+  }
+  return result
+}
+
+const f = function(x) { return x * x * x }
+let numbers = [0, 1, 2, 3, 4]
+
+console.log(map(f, numbers))
+
