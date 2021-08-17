f0bf30896de8a133309a975326dbe6abceda9e1f
tommy.huang <cy94295@gmail.com>
2021-08-17 09:42:46 +0800

    update

--- sikuli/polkamonster_1080.sikuli/polkamonster_1080.py
+++ sikuli/polkamonster_1080.sikuli/polkamonster_1080.py
@@ -5,14 +5,16 @@ nextCount = 0
 noFightCount = 0
 setThrowException(False)
 
+
 while True:
-    timeArea = Region(58,786,394,65)
-    isTimeArea = timeArea.exists(Pattern("1629013105493.png").similar(0.97), 5)
+    timeArea = Region(1,384,524,662)
+    isTimeArea = timeArea.exists(Pattern("1629084760954.png").similar(0.9), 5)
     if isTimeArea is None:
         print('沒 Next Time! 開始打架!')
+        wait(10)
     
-        sixtyMonsterArea =Region(516,880,236,52)
-        isSixtyMonsterArea = sixtyMonsterArea.exists(Pattern("1629013141015.png").similar(0.98), 5)
+        sixtyMonsterArea = Region(480,814,324,162)
+        isSixtyMonsterArea = sixtyMonsterArea.exists(Pattern("1629013141015.png").similar(0.9), 5)
         if isSixtyMonsterArea is None:
             print('沒找到 Fight 按鈕')
 
@@ -25,7 +27,7 @@ while True:
         else:
             print('找到 Fight 開始打架!')
             isSixtyMonsterArea.highlight(2)
-            sixtyMonsterArea.click(Pattern("1629013141015.png").similar(0.98))
+            sixtyMonsterArea.click(Pattern("1629013141015.png").similar(0.9))
             
             metaArea = Region(731,516,348,85)
             isMetaArea = metaArea.exists(Pattern("1629013168898.png").similar(0.98), 5)
@@ -50,8 +52,8 @@ while True:
         print('找到 Next Time!')
         isTimeArea.highlight(2)
 
-        nextArea = Region(92,861,355,78)
-        isNextArea = nextArea.exists(Pattern("1629013326373.png").similar(0.97), 5)
+        nextArea = Region(25,807,460,123)
+        isNextArea = nextArea.exists(Pattern("1629013326373.png").similar(0.92), 5)
         if isNextArea is None:
             print('沒找到下一個按鈕')
 
@@ -62,11 +64,11 @@ while True:
                 wait(5)
         else:
             print('切換到下一個!')
-            nextArea.click(Pattern("1629013326373.png").similar(0.97))
+            nextArea.click(Pattern("1629013326373.png").similar(0.92))
             isNextArea.highlight(2)
             nextCount += 1
             print(nextCount)
-            if(nextCount % 5) == 0:
+            if(nextCount % 10) == 0:
                 type(Key.F5) 
                 wait(5)
             mouseMove(-100,0)
