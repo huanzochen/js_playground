f4a38a652b1014b6b218ef12a52694f0aa500b4a
tommy.huang <cy94295@gmail.com>
2021-08-20 09:13:34 +0800

    update

--- sikuli/polkamonster_1080.sikuli/polkamonster_1080.py
+++ sikuli/polkamonster_1080.sikuli/polkamonster_1080.py
@@ -5,6 +5,17 @@ nextCount = 0
 noFightCount = 0
 setThrowException(False)
 
+def reloadPage():
+    type(Key.F5)
+    reloadArea = Region(1,104,1073,256)
+    isReloadArea = reloadArea.exists(Pattern("1629421744929.png").similar(0.9), 30)
+    if isReloadArea is None:
+        print('沒有載入出網頁, 稍等重整')
+        reloadPage()
+    else:
+        print('載入網頁成功!')
+        wait(5)
+    
 
 while True:
     timeArea = Region(1,384,524,662)
@@ -13,7 +24,7 @@ while True:
         print('沒 Next Time! 開始打架!')
         wait(10)
     
-        sixtyMonsterArea = Region(480,814,324,162)
+        sixtyMonsterArea = Region(480,606,385,544)
         isSixtyMonsterArea = sixtyMonsterArea.exists(Pattern("1629013141015.png").similar(0.9), 5)
         if isSixtyMonsterArea is None:
             print('沒找到 Fight 按鈕')
@@ -21,8 +32,7 @@ while True:
             noFightCount += 1
             print(noFightCount)
             if(noFightCount % 2) == 0:
-                type(Key.F5) 
-                wait(5)
+                reloadPage()
             
         else:
             print('找到 Fight 開始打架!')
@@ -37,22 +47,20 @@ while True:
                 noFightCount += 1
                 print(noFightCount)
                 if(noFightCount % 2) == 0:
-                    type(Key.F5) 
-                    wait(5)
+                    reloadPage()
 
             else:
                 print('找到meta, 點擊!')
                 isMetaArea.highlight(2)
                 metaArea.click(Pattern("1629013168898.png").similar(0.98))
                 wait(30)
-                type(Key.F5)
-                wait(5)
+                reloadPage()
     
     else:
         print('找到 Next Time!')
         isTimeArea.highlight(2)
 
-        nextArea = Region(25,807,460,123)
+        nextArea = Region(3,230,560,971)
         isNextArea = nextArea.exists(Pattern("1629013326373.png").similar(0.92), 5)
         if isNextArea is None:
             print('沒找到下一個按鈕')
@@ -60,8 +68,7 @@ while True:
             noFightCount += 1
             print(noFightCount)
             if(noFightCount % 3) == 0:
-                type(Key.F5) 
-                wait(5)
+                reloadPage()
         else:
             print('切換到下一個!')
             nextArea.click(Pattern("1629013326373.png").similar(0.92))
@@ -69,7 +76,6 @@ while True:
             nextCount += 1
             print(nextCount)
             if(nextCount % 10) == 0:
-                type(Key.F5) 
-                wait(5)
+                reloadPage()
             mouseMove(-100,0)
     
