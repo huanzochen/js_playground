296409985f87013cd7b7b5ac3df751fa6110945e
tzuchuan.huang <cy94295@gmail.com>
2024-06-13 22:55:39 +0800

    feat: simple binary heap min

---
+++ js_practice/basics/plus_minus_symbol.js
@@ -0,0 +1,11 @@
+let a = 0
+let b = 0
+
+
+
+console.log('a++, it should be add after display, a++:', a++)
+console.log('++a, it should be added before display, ++a:', ++a)
+console.log('a++, it should be add after display, a++:', a++)
+console.log('++a, it should be added before display, ++a:', ++a)
+console.log('a++, it should be add after display, a++:', a++)
+console.log('++a, it should be added before display, ++a:', ++a)
