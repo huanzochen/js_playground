3941961c1d7405195a250b1063673f38c1e5fb87
tzuchuan.huang <cy94295@gmail.com>
2024-06-25 00:45:44 +0800

    feat: mergeSort2

--- js_practice/algorithm/sort/merge_sort.js
+++ js_practice/algorithm/sort/merge_sort.js
@@ -28,7 +28,8 @@ const merge = (a, b) => {
 // put in two sorted array to test
 // console.log(merge([1, 3, 4, 19], [2, 5, 7, 10, 11, 45]))
 
-const mergeSort = (nums) => {
+// The BreakDown just wants to let you know how the merge sort works.
+const mergeSortBreakDown = (nums) => {
   if (nums.length <= 1) {
     console.log('nums:', nums)
     return nums
@@ -37,26 +38,86 @@ const mergeSort = (nums) => {
   let mid = Math.floor(nums.length / 2)
 
   if (nums.length === 2) {
-    let left = mergeSort(nums.slice(0, mid))
-    let right = mergeSort(nums.slice(mid, nums.length))
+    let left = mergeSortBreakDown(nums.slice(0, mid))
+    let right = mergeSortBreakDown(nums.slice(mid, nums.length))
     console.log('2 --- left:, right:', left, right)
     return merge(left, right)
   }
 
   if (nums.length === 4) {
-    let left = mergeSort(nums.slice(0, mid))
-    let right = mergeSort(nums.slice(mid, nums.length))
+    let left = mergeSortBreakDown(nums.slice(0, mid))
+    let right = mergeSortBreakDown(nums.slice(mid, nums.length))
     console.log('4 --- left:, right:', left, right)
     return merge(left, right)
   }
 
   if (nums.length === 8) {
-    let left = mergeSort(nums.slice(0, mid))
-    let right = mergeSort(nums.slice(mid, nums.length))
+    let left = mergeSortBreakDown(nums.slice(0, mid))
+    let right = mergeSortBreakDown(nums.slice(mid, nums.length))
     console.log('8 --- left:, right:', left, right)
     return merge(left, right)
   }
 
 }
 
-console.log('mergeSort:', mergeSort([2, 7, 8, 3, 12, 34, 5, 78]))
+// console.log('mergeSortBreakDown:', mergeSortBreakDown([2, 7, 8, 3, 12, 34, 5, 78]))
+
+const mergeSort = (nums) => {
+  if (nums.length <= 1) {
+    return nums
+  }
+
+  let mid = Math.floor(nums.length / 2)
+  let left = mergeSort(nums.slice(0, mid))
+  let right = mergeSort(nums.slice(mid, nums.length))
+  return merge(left, right)
+
+}
+// console.log('mergeSort:', mergeSort([22, 3, 4, 66, 21, 12, 456, 34, 67, 88]))
+
+const merge2 = (nums, lo, mid, hi) => {
+  console.log('lo, mid, hi:', lo, mid, hi)
+
+  let aI = lo
+  let bI = mid + 1
+
+  let res = []
+
+  // 注意這是 左閉右閉 區間
+  while (aI <= mid && bI <= hi) {
+    if (nums[aI] < nums[bI]) {
+      res.push(nums[aI])
+      aI++
+    }
+    else {
+      res.push(nums[bI])
+      bI++
+    }
+  }
+  if (aI <= mid) {
+    res.push(...nums.slice(aI, mid + 1))
+  } else if (bI <= hi) {
+    res.push(...nums.slice(bI, hi + 1))
+  }
+
+  for (let i = lo; i <= hi; i++) {
+    nums[i] = res[i - lo]
+  }
+}
+
+const mergeSort2 = (nums, lo, hi) => {
+  if (lo >= hi) { return }
+
+  const mid = Math.floor((lo + hi) / 2)
+
+  mergeSort2(nums, lo, mid)
+  mergeSort2(nums, mid + 1, hi)
+
+  merge2(nums, lo, mid, hi)
+}
+
+const nums2 = [22, 3, 4, 66, 21, 12, 456, 34, 67, 88]
+
+mergeSort2(nums2, 0, nums2.length - 1)
+
+console.log('nums2 by mergeSort2, nums2:', nums2)
